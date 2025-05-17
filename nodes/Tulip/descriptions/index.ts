91ab6f4ee950d1563ff35e0f7bfcd3ee2e543eb9
Clément Fayard <clementfayard@hotmail.com>
2025-05-16 22:26:06 -0700

    code cleanup

--- nodes/Tulip/descriptions/index.ts
+++
@@ -1,9 +0,0 @@
-import { tableFields, tableOperations } from './TableDescription';
-import { machineFields, machineOperations } from './MachineDescription';
-
-export {
-	tableFields as tableDescription,
-	tableOperations as tableOperation,
-	machineFields as machineDescription,
-	machineOperations as machineOperation,
-};
