e09ee5f9d87bea503c1a8e7f9c53aa16942d8489
Clément Fayard <clementfayard@hotmail.com>
2025-05-19 12:41:47 -0400

    Add the create table execute function

--- nodes/Tulip/actions/table/createTable/execute.ts
+++ nodes/Tulip/actions/table/createTable/execute.ts
@@ -8,6 +8,32 @@ export async function execute(this: IExecuteFunctions, index: number): Promise<I
 	const requestMethod = 'POST';
 	const endpoint = 'tables';
 
+	// Get the main table properties
+	body.label = this.getNodeParameter('tableLabel', index) as string;
+	body.description = this.getNodeParameter('description', index) as string;
+
+	// Get the columns configuration
+	const columnsData = this.getNodeParameter('columns.columnValues', index, []) as Array<{
+			columnName: string;
+			label: string;
+			description: string;
+			dataType: string;
+			hidden: boolean;
+			unique: boolean;
+	}>;
+
+	// Format columns data
+	body.columns = columnsData.map(column => ({
+			name: column.columnName,
+			label: column.label,
+			description: column.description,
+			dataType: {
+					type: column.dataType,
+			},
+			hidden: column.hidden,
+			unique: column.unique,
+	}));
+
 	const responseData = await apiRequest.call(this, requestMethod, endpoint, body, qs);
 
 	return this.helpers.returnJsonArray(responseData as IDataObject[]);
