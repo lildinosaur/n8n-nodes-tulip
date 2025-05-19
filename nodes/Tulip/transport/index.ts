4f5bf48b9ffe5b394af46c31f6f0e564b022428b
Clément Fayard <clementfayard@hotmail.com>
2025-05-19 13:36:54 -0400

    clean http request

--- nodes/Tulip/transport/index.ts
+++ nodes/Tulip/transport/index.ts
@@ -33,9 +33,5 @@ export async function apiRequest(
 		resolveWithFullResponse: resolveWithFullResponse,
 	};
 
-	try {
-		return await this.helpers.requestWithAuthentication.call(this, 'tulipApi', options);
-	} catch (error) {
-		throw new NodeApiError(this.getNode(), error as JsonObject);
-	}
+	return await this.helpers.requestWithAuthentication.call(this, 'tulipApi', options);
 }
